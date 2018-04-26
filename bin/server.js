880ad9338c43c091869248b854cadd7a6c944528
Eduardo Mendonca <eduardo@smnti.com.br>
2018-04-26 17:38:03 -0300

    Melhorias aplicadas

--- bin/server.js
+++ bin/server.js
@@ -1,63 +1,90 @@
-const app = require('../src/app');
-const debug = require('debug')('nodeapi:server');
-const http = require('http');
+const app = require("../src/app");
+const debug = require("debug")("nodeapi:server");
+const http = require("http");
+var express = require("express");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
 //const webconfig = require('../webconfig');
 
-const port = normalizePort(process.env.PORT || '3001');
-app.set('port', port);
+const port = normalizePort(process.env.PORT || "3001");
+app.set("port", port);
 
 const server = http.createServer(app);
 
 server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-console.log('Servidor rodando em http://localhost:' + port)
+server.on("error", onError);
+server.on("listening", onListening);
+console.log("Servidor rodando em http://localhost:" + port);
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        return val;
-    }
+  if (isNaN(port)) {
+    return val;
+  }
 
-    if (port >= 0) {
-        return port;
-    }
+  if (port >= 0) {
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
-function onError(error){
-    if(error.syscall !== 'listen'){
-        throw error;
-    }
-    const bind = typeof port === 'string'
-        ? 'Pipe' + port
-        : 'Port' + port;
-    
-    switch (error.code){
-        case 'EACCES':
-            console.error(bind + 'require elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + 'is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+function onError(error) {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "Pipe" + port : "Port" + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + "require elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + "is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 function onListening() {
   const addr = server.address();
-  const bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
 }
 
 /*app.listen(webconfig.portApi, () => {
     console.log(`[${webconfig.nameApi}] rodando em: ${webconfig.urlApi}:${webconfig.portApi}`)
 });*/
 
+var schema = buildSchema(`
+  type Produto {
+    ss: String
+    eduardo: [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+  eduardo: () => {
+    return [1, 2, 3, 4].map(_ => 1 + Math.floor(Math.random() * 6));
+  }
+};
+
+var ap = express();
+ap.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+  })
+);
+ap.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-'use strict'
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const config = require('./config');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("./config");
+var graphqlHTTP = require("express-graphql");
+var { buildSchema } = require("graphql");
 const app = express();
 const router = express.Router();
 
@@ -12,33 +13,37 @@ const router = express.Router();
 mongoose.connect(config.connectionString);
 
 // Carrega os Models
-const Produto = require('./models/produto');
-const Cliente = require('./models/cliente');
-const Ordem = require('./models/ordem');
+const Produto = require("./models/produto");
+const Cliente = require("./models/cliente");
+const Ordem = require("./models/ordem");
 
 //carrega as rotas
-const indexRoute = require('./routers/index-route');
-const produtoRoute = require('./routers/produto-route');
-const clienteRoute = require('./routers/cliente-route');
-const ordemRoute = require('./routers/ordem-route');
+const indexRoute = require("./routers/index-route");
+const produtoRoute = require("./routers/produto-route");
+const clienteRoute = require("./routers/cliente-route");
+const ordemRoute = require("./routers/ordem-route");
 
-app.use(bodyParser.json({
-    limit: '5mb'
-}));
+app.use(
+  bodyParser.json({
+    limit: "5mb"
+  })
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Habilita o CORS
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-acess-token');
-    res.header('Acess-Control.Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-acess-token"
+  );
+  res.header("Acess-Control.Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
 });
 
-app.use('/', indexRoute);
-app.use('/produtos', produtoRoute);
-app.use('/clientes', clienteRoute);
-app.use('/ordens', ordemRoute);
-
+app.use("/", indexRoute);
+app.use("/produtos", produtoRoute);
+app.use("/clientes", clienteRoute);
+app.use("/ordens", ordemRoute);
 
 module.exports = app;
